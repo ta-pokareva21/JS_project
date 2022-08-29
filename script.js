8cf5498843b3376adacf0792a3ca8b048123b9f0
Tanya <tanyapokareva2002@gmail.com>
2022-08-30 00:45:03 +0400

    Fourth task commit

--- script.js
+++ script.js
@@ -1,71 +1,75 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-    }
-}
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?','').trim(),
+                  b = +prompt('Насколько оцените его?','').trim();
+            if ( a != null && b != null &&  a != '' && a.length <=50 && b !=''){
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error')
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10){
+            console.log("Просмотрено довольно мало фильмов");
+        }
+        else if(personalMovieDB.count >= 10 && personalMovieDB.count <=30){
+            console.log("Вы классический зритель");
+        }
+        else if(personalMovieDB.count > 30){
+            console.log("Вы киноман!");
+        }else{
+            console.log("Произошла ошибка")}
+        
+    },
+    showMyDB: function (hidden) {
+        if(!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if(personalMovieDB.private){
+            personalMovieDB.private = false;
+        }else {
+            personalMovieDB.private = true;
+        }
+    },
+    writeYourGenres: function() {
+        let genres=[];
+        for(let i = 1; i < 4; i++){
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`); 
+            if( genre === '' || genre === null){
+                console.log("Вы ввели некорректные данные или не ввели их вовсе");
+                i--;
+            }else {
+            personalMovieDB.genres[i-1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item,i) => {
+            console.log(`Любимый жанр ${i+1} - это ${item}`);
+        });
+    }
 };
 
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?',''),
-              b = +prompt('Насколько оцените его?','');
-        if ( a != null && b != null &&  a != '' && a.length <=50 && b !=''){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error')
-            i--;
-        }
-    }
-}
 
-rememberMyFilms();
 
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10){
-        console.log("Просмотрено довольно мало фильмов");
-    }
-    else if(personalMovieDB.count >= 10 && personalMovieDB.count <=30){
-        console.log("Вы классический зритель");
-    }
-    else if(personalMovieDB.count > 30){
-        console.log("Вы киноман!");
-    }else{
-        console.log("Произошла ошибка")}
-    
-}
 
-detectPersonalLevel();
 
-//console.log(personalMovieDB);
-
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.private);
-
-function writeYourGenres(){
-    let genres=[];
-    for(let i = 1; i < 4; i++){
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`); 
-    }
-}
-
-writeYourGenres();
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,20 @@
-const str = "idi nahyi huesos";
-console.log(str.substring(0));
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = "Доступные валюты:"+"\n";
+    
+    if(arr.length===0){
+        return 'Нет доступных валют';
+    }
+    let a = [];
+    for (let i = 0; i< arr.length; i++){
+        if(arr[i]===missingCurr){
+             arr.splice(i,1) ;
+        }
+    }
+    str += arr.join("\n");
+    return str ; 
+}
+
+console.log(availableCurr(additionalCurrencies, 'UAH'));
